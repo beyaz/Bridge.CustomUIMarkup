b24f6e1381655bfd6efa095cde3cb7b34ccdd033
test <beyaz1404@hotmail.com>
2018-01-29 12:14:31 +0300

    .

---
+++ Test/Bridge.CustomUIMarkup.Test/Test.js
@@ -0,0 +1,29 @@
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/js/jquery-2.2.4.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/Lib/qunit/qunit.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/Lib/semantic-ui/semantic.js" />
+
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/Lib/semantic-ui-DataTable/jquery.dataTables.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/Lib/semantic-ui-DataTable/dataTables.semanticui.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/Lib/semantic-ui-DataTable/dataTables.TR.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/Lib/semantic-ui-calendar/calendar.js" />
+
+
+
+
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/js/bridge.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/js/bridge.console.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/js/bridge.meta.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/js/newtonsoft.json.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/js/Bridge.CustomUIMarkup.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/js/Bridge.CustomUIMarkup.meta.js" />
+
+
+
+/// <reference path="D:/github/Bridge.CustomUIMarkup/Test/Bridge.CustomUIMarkup.Test/bin/Debug/bridge/Bridge.CustomUIMarkup.Test.js" />
+/// <reference path="D:/github/Bridge.CustomUIMarkup/Test/Bridge.CustomUIMarkup.Test/bin/Debug/bridge/Bridge.CustomUIMarkup.Test.meta.js" />
+
+/// <reference path="D:/github/Bridge.CustomUIMarkup/bin/Lib/semantic-ui/semantic.css" />
+
+
+
+
