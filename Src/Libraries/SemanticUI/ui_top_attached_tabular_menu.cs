c58b9a278c68d1ebccc82cc5f8c6d3cc86db9b99
test <beyaz1404@hotmail.com>
2018-02-10 19:44:51 +0300

    .

--- Src/Libraries/SemanticUI/ui_top_attached_tabular_menu.cs
+++ Src/Libraries/SemanticUI/ui_top_attached_tabular_menu.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using Retyped;
 
 namespace Bridge.CustomUIMarkup.Libraries.SemanticUI
 {
