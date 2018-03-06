4eebe49bdd38d152a974ccbc6b09fbe67c3d80b5
test <beyaz1404@hotmail.com>
2018-03-06 14:12:55 +0300

    .

--- Bridge.CustomUIMarkup.Test/CastTest.cs
+++ Bridge.CustomUIMarkup.Test/CastTest.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Bridge.Html5;
+using System.Globalization;
 using Bridge.QUnit;
 
 namespace Bridge.CustomUIMarkup.Test
@@ -10,10 +10,22 @@ namespace Bridge.CustomUIMarkup.Test
         public static void Register()
         {
             QUnit.QUnit.Test(nameof(CastTest) + "->" + nameof(Int32ToNullableInt32), Int32ToNullableInt32);
+            QUnit.QUnit.Test(nameof(CastTest) + "->" + nameof(DateParseTest), DateParseTest);
         }
         #endregion
 
         #region Methods
+        static void DateParseTest(Assert assert)
+        {
+            var result = Cast.To<DateTime?>("2015/03/13");
+
+            assert.Ok(result != null);
+
+            assert.Equal(2015, result.GetValueOrDefault().Year);
+            assert.Equal(3, result.GetValueOrDefault().Month);
+            assert.Equal(13, result.GetValueOrDefault().Day);
+        }
+
         static void Int32ToNullableInt32(Assert assert)
         {
             var i = 6;
